3099d70a42d4cf5db87b5c0096ddecc9be11681a
glq1218 <glq05201218@163.com>
2022-08-28 00:35:26 +0800

    1

--- gblog-blog/src/main/java/com/glq1218/GBlogApplication.java
+++ gblog-blog/src/main/java/com/glq1218/GBlogApplication.java
@@ -1,9 +1,17 @@
 package com.glq1218;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author: glq
  * @Data: 2022/8/27 下午10:53
  * @Description: TODO
  */
+
+@SpringBootApplication
 public class GBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GBlogApplication.class,args);
+    }
 }
